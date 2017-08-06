e60598fcae4a1cd6a6513657f5406a233697462b
Olivier Hoareau <olivier@phppro.fr>
2017-08-06 21:40:03 +0200

    abstract model

---
+++ src/Model/Base/AbstractModel.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the COMMON package.
+ *
+ * (c) itiQiti SAS <opensource@itiqiti.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Itq\Common\Model\Base;
+
+use Itq\Common\ModelInterface;
+
+/**
+ * @author itiQiti Dev Team <opensource@itiqiti.com>
+ */
+abstract class AbstractModel implements ModelInterface
+{
+}
