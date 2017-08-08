917ee9990833cf486bf0ae24cdba4a8c0254833c
Olivier Hoareau <olivier@phppro.fr>
2017-08-08 17:25:25 +0200

    add error translations

--- tests/Itq/Common/ErrorManagerTest.php
+++ tests/Itq/Common/ErrorManagerTest.php
@@ -16,6 +16,7 @@ use Itq\Common\Exception\ErrorException;
 
 use Itq\Common\Tests\Base\AbstractTestCase;
 
+use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Translation\Translator;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
 
@@ -139,6 +140,142 @@ class ErrorManagerTest extends AbstractTestCase
         $this->assertEquals(0, $e->getApplicationCode());
         $this->assertEquals('service.method.unknown', $e->getApplicationKey());
         $this->assertEquals(['A', 'b'], $e->getApplicationParams());
+    }
+    /**
+     * @group integ
+     */
+    public function testCreateExceptionForRealTranslatorWithTranslation()
+    {
+        $translator = new Translator('fr_FR');
+        $translator->addLoader('yml', new YamlFileLoader());
+        $translator->addResource('yml', __DIR__.'/../../../src/Itq/Bundle/ItqBundle/Resources/translations/errors.fr.yml', 'fr_FR', 'errors');
+        $translator->addResource('yml', __DIR__.'/../../../src/Itq/Bundle/ItqBundle/Resources/translations/errors.en.yml', 'en_US', 'errors');
+        $errorManager = new ErrorManager($translator);
 
+        /** @var ErrorException $e */
+
+        // fr_FR (default)
+        $e = $errorManager->createException('service.method.unknown', ['A', 'b']);
+
+        $this->assertTrue($e instanceof ErrorException);
+        $this->assertEquals('Méthode A::b() non disponible', $e->getMessage());
+        $this->assertEquals(500, $e->getCode());
+        $this->assertEquals(0, $e->getApplicationCode());
+        $this->assertEquals('service.method.unknown', $e->getApplicationKey());
+        $this->assertEquals(['A', 'b'], $e->getApplicationParams());
+
+        // en_US
+        $translator->setLocale('en_US');
+
+        $e = $errorManager->createException('service.method.unknown', ['A', 'b']);
+
+        $this->assertTrue($e instanceof ErrorException);
+        $this->assertEquals('Unknown method A::b()', $e->getMessage());
+        $this->assertEquals(500, $e->getCode());
+        $this->assertEquals(0, $e->getApplicationCode());
+        $this->assertEquals('service.method.unknown', $e->getApplicationKey());
+        $this->assertEquals(['A', 'b'], $e->getApplicationParams());
+
+    }
+    /**
+     * @group integ
+     */
+    public function testCreateExceptionForRealTranslatorWithMissingTranslation()
+    {
+        $translator = new Translator('fr_FR');
+        $translator->addLoader('yml', new YamlFileLoader());
+        $translator->addResource('yml', __DIR__.'/../../../src/Itq/Bundle/ItqBundle/Resources/translations/errors.en.yml', 'en_US', 'errors');
+        $errorManager = new ErrorManager($translator);
+
+        /** @var ErrorException $e */
+
+        // fr_FR (default)
+        $e = $errorManager->createException('service.method.unknown', ['A', 'b']);
+
+        $this->assertTrue($e instanceof ErrorException);
+        $this->assertEquals('service.method.unknown', $e->getMessage());
+        $this->assertEquals(500, $e->getCode());
+        $this->assertEquals(0, $e->getApplicationCode());
+        $this->assertEquals('service.method.unknown', $e->getApplicationKey());
+        $this->assertEquals(['A', 'b'], $e->getApplicationParams());
+    }
+    /**
+     * @group integ
+     */
+    public function testCreateExceptionForRealTranslatorWithTranslationAndAppCode()
+    {
+        $translator = new Translator('fr_FR');
+        $translator->addLoader('yml', new YamlFileLoader());
+        $translator->addResource('yml', __DIR__.'/../../../src/Itq/Bundle/ItqBundle/Resources/translations/errors.fr.yml', 'fr_FR', 'errors');
+        $errorManager = new ErrorManager($translator);
+
+        /** @var ErrorException $e */
+
+        // fr_FR (default)
+        $e = $errorManager->createException('#1010:service.method.unknown', ['A', 'b']);
+
+        $this->assertTrue($e instanceof ErrorException);
+        $this->assertEquals('Méthode A::b() non disponible', $e->getMessage());
+        $this->assertEquals(500, $e->getCode());
+        $this->assertEquals(1010, $e->getApplicationCode());
+        $this->assertEquals('service.method.unknown', $e->getApplicationKey());
+        $this->assertEquals(['A', 'b'], $e->getApplicationParams());
+
+    }
+    /**
+     * @group integ
+     */
+    public function testCreateExceptionForRealTranslatorWithTranslationAndMappedAppCode()
+    {
+        $translator = new Translator('fr_FR');
+        $translator->addLoader('yml', new YamlFileLoader());
+        $translator->addResource('yml', __DIR__.'/../../../src/Itq/Bundle/ItqBundle/Resources/translations/errors.fr.yml', 'fr_FR', 'errors');
+        $errorManager = new ErrorManager($translator);
+        $errorManager->setKeyCodeMapping(['service.method.unknown' => 1111]);
+
+        /** @var ErrorException $e */
+
+        // fr_FR (default)
+        $e = $errorManager->createException('service.method.unknown', ['A', 'b']);
+
+        $this->assertTrue($e instanceof ErrorException);
+        $this->assertEquals('Méthode A::b() non disponible', $e->getMessage());
+        $this->assertEquals(500, $e->getCode());
+        $this->assertEquals(1111, $e->getApplicationCode());
+        $this->assertEquals('service.method.unknown', $e->getApplicationKey());
+        $this->assertEquals(['A', 'b'], $e->getApplicationParams());
+
+        $e = $errorManager->createException('#22:service.method.unknown', ['A', 'b']);
+
+        $this->assertTrue($e instanceof ErrorException);
+        $this->assertEquals('Méthode A::b() non disponible', $e->getMessage());
+        $this->assertEquals(500, $e->getCode());
+        $this->assertEquals(22, $e->getApplicationCode());
+        $this->assertEquals('service.method.unknown', $e->getApplicationKey());
+        $this->assertEquals(['A', 'b'], $e->getApplicationParams());
+
+    }
+    /**
+     * @group integ
+     */
+    public function testCreateExceptionForRealTranslatorWithTranslationAndRealMappedAppCode()
+    {
+        $translator = new Translator('fr_FR');
+        $translator->addLoader('yml', new YamlFileLoader());
+        $translator->addResource('yml', __DIR__.'/../../../src/Itq/Bundle/ItqBundle/Resources/translations/errors.fr.yml', 'fr_FR', 'errors');
+        $errorManager = new ErrorManager($translator);
+        $errorManager->setKeyCodeMapping(Yaml::parse(file_get_contents(__DIR__.'/../../../src/Itq/Bundle/ItqBundle/Resources/config/error-mapping.yml')));
+
+        /** @var ErrorException $e */
+
+        // fr_FR (default)
+        $e = $errorManager->createException('service.method.unknown', ['A', 'b']);
+
+        $this->assertTrue($e instanceof ErrorException);
+        $this->assertEquals('Méthode A::b() non disponible', $e->getMessage());
+        $this->assertEquals(500, $e->getCode());
+        $this->assertEquals(11010, $e->getApplicationCode());
+        $this->assertEquals('service.method.unknown', $e->getApplicationKey());
+        $this->assertEquals(['A', 'b'], $e->getApplicationParams());
     }
 }
