9f5ded503c97b38068b67167fae4a577c0e82bff
Olivier Hoareau <olivier@phppro.fr>
2017-08-30 19:05:02 +0200

    up

---
+++ src/Itq/Bundle/ItqBundle/Controller/SupervisionController.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the COMMON package.
+ *
+ * (c) itiQiti SAS <opensource@itiqiti.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Itq\Bundle\ItqBundle\Controller;
+
+use Itq\Common\Traits;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Itq\Common\Controller\Base\AbstractCrudController;
+
+/**
+ * Supervision controller.
+ *
+ * @author itiQiti Dev Team <opensource@itiqiti.com>
+ */
+class SupervisionController extends AbstractCrudController
+{
+    use Traits\Controller\ServiceAware\SupervisionServiceAwareControllerTrait;
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function pingAction(Request $request)
+    {
+        return $this->returnGetResponse($request, $this->getSupervisionService()->supervise());
+    }
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function aliveAction(Request $request)
+    {
+        return $this->returnResponse('OK', 200, ['Content-Type' => 'text/plain'], [], $request);
+    }
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function whoamiAction(Request $request)
+    {
+        return $this->returnGetResponse($request, $this->getSupervisionService()->identify());
+    }
+}
