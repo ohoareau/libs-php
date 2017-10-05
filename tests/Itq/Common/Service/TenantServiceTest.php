da19be18a34d506227b4d15766bcea33ce4a159e
Michel MAIER <michel.maier@gmail.com>
2017-10-05 17:12:43 +0200

    adds tests

--- tests/Itq/Common/Service/TenantServiceTest.php
+++ tests/Itq/Common/Service/TenantServiceTest.php
@@ -11,6 +11,8 @@
 
 namespace Tests\Itq\Common\Service;
 
+use Itq\Common\Service\TenantService;
+use Itq\Common\Aware\TenantAwareInterface;
 use Itq\Common\Tests\Service\Base\AbstractServiceTestCase;
 
 /**
@@ -22,6 +24,15 @@ use Itq\Common\Tests\Service\Base\AbstractServiceTestCase;
 class TenantServiceTest extends AbstractServiceTestCase
 {
     /**
+     * @return TenantService
+     */
+    public function s()
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+
+        return parent::s();
+    }
+    /**
      * @return array
      */
     public function constructor()
@@ -29,6 +40,47 @@ class TenantServiceTest extends AbstractServiceTestCase
         return [
             $this->mockedTokenStorage(),
             'testtenant',
+        ];
+    }
+    /**
+     * @group unit
+     */
+    public function testGetFullType()
+    {
+        $this->assertEquals('tenant', $this->s()->getFullType());
+    }
+    /**
+     * @param string|null $tenant
+     * @param string      $defaultToken
+     * @param string      $expected
+     *
+     * @group unit
+     * @dataProvider provideGetCurrentData
+     */
+    public function testGetCurrent($tenant, $defaultToken, $expected)
+    {
+        if (null !== $tenant) {
+            $this->mocked('tenantAware', TenantAwareInterface::class);
+            $this->mocked('tenantAware')->expects($this->once())->method('getTenant')->will($this->returnValue($tenant));
+
+            $tokenStorageReturn = $this->mocked('tenantAware');
+        } else {
+            $tokenStorageReturn = null;
+        }
+
+        $this->mockedTokenStorage()->expects($this->once())->method('getToken')->will($this->returnValue($tokenStorageReturn));
+
+        $this->s()->setDefault($defaultToken);
+        $this->assertEquals($expected, $this->s()->getCurrent());
+    }
+    /**
+     * @return array
+     */
+    public function provideGetCurrentData()
+    {
+        return [
+            ['token1', 'default_tenant', 'token1'],
+            [ null, 'default_tenant', 'default_tenant'],
         ];
     }
 }
