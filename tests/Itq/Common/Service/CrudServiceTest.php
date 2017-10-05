dcf5af8e6f348854257ed53f558bfc352e9c2cca
augustin merle <augustin@laveaucoupet.fr>
2017-10-05 11:24:48 +0200

    complete CrudService class test

--- tests/Itq/Common/Service/CrudServiceTest.php
+++ tests/Itq/Common/Service/CrudServiceTest.php
@@ -11,6 +11,9 @@
 
 namespace Tests\Itq\Common\Service;
 
+use stdClass;
+use Itq\Common\Service\CrudService;
+use PHPUnit_Framework_MockObject_MockObject;
 use Itq\Common\Tests\Service\Base\AbstractServiceTestCase;
 
 /**
@@ -21,4 +24,60 @@ use Itq\Common\Tests\Service\Base\AbstractServiceTestCase;
  */
 class CrudServiceTest extends AbstractServiceTestCase
 {
+    /**
+     * @return CrudService | PHPUnit_Framework_MockObject_MockObject
+     */
+    public function s()
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+
+        return parent::s();
+    }
+
+
+    public function testAdd()
+    {
+        $expected = new stdClass;
+        $this->s()
+            ->expects($this->once())->method('setArrayParameterKey')
+            ->with('crudServices', 'tutu', $expected)
+            ->willReturn($this->s())
+        ;
+        $this->assertSame($this->s(), $this->s()->add('tutu', $expected ));
+
+    }
+
+    public function testGet()
+    {
+        $expected = new stdClass;
+        $this->s()->expects($this->once())->method('getArrayParameterKey')->will($this->returnValue($expected));
+        $this->assertSame($expected, $this->s()->get( 'toto' ));
+
+    }
+
+    public function testGetAll()
+    {
+        $expected = new stdClass;
+        $this->s()->expects($this->once())->method('getArrayParameter')->will($this->returnValue($expected));
+        $this->assertSame($expected, $this->s()->getAll());
+
+    }
+
+    public function testHas()
+    {
+        $expected = new stdClass;
+        $this->s()->expects($this->once())->method('hasArrayParameterKey')->will($this->returnValue($expected));
+        $this->assertSame($expected, $this->s()->has('toto'));
+    }
+
+
+    /**
+     * @return array
+     */
+    protected function getMockedMethod()
+    {
+        return ['setArrayParameterKey', 'getArrayParameterKey', 'getArrayParameter', 'hasArrayParameterKey'];
+    }
+
+
 }
