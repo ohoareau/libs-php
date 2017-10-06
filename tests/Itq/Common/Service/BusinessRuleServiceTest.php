ed5a12bf731a0741ad0ba4137a0f607fb8067584
augustin merle <augustin@laveaucoupet.fr>
2017-10-06 10:17:28 +0200

    comptete BusinessRuleServiceTest.php

--- tests/Itq/Common/Service/BusinessRuleServiceTest.php
+++ tests/Itq/Common/Service/BusinessRuleServiceTest.php
@@ -11,7 +11,9 @@
 
 namespace Tests\Itq\Common\Service;
 
+use Itq\Common\ValidationContext;
 use Itq\Common\Service\BusinessRuleService;
+use Itq\Common\Exception\BusinessRuleException;
 use Itq\Common\Tests\Service\Base\AbstractServiceTestCase;
 
 /**
@@ -62,49 +64,133 @@ class BusinessRuleServiceTest extends AbstractServiceTestCase
      */
     public function testExecuteModelOperationBusinessRulesExecuteAllBusinessRulesInRegisteredOrder()
     {
-        $this->mockedTenantService()->expects($this->any())->method('getCurrent')->will($this->returnValue('testtenant'));
-        $context = (object) ['counter' => 0, 'value' => 0];
-
-        $brX001 = function () use ($context) {
-            $context->counter++;
-            $context->value += 1;
-        };
-        $brX002 = function () use ($context) {
-            $context->counter++;
-            $context->value /= 2;
-        };
-
-        $this->s()->register('X001', 'my first business rule', $brX001, ['model' => 'myModel', 'operation' => 'create']);
-        $this->s()->register('X002', 'my second business rule', $brX002, ['model' => 'myModel', 'operation' => 'create']);
-
+        $context = $this->getRegisteredService();
         $this->s()->executeBusinessRulesForModelOperation('myModel', 'create', (object) []);
 
         $this->assertEquals(2, $context->counter);
-        $this->assertEquals(0.5, $context->value);
+        $this->assertEquals(3, $context->value);
+
     }
     /**
      * @group unit
      */
     public function testExecuteModelOperationBusinessRulesExecuteAllBusinessRulesForTheSpecifiedTenantInRegisteredOrder()
     {
+        $context = $this->getRegisteredService();
+        $this->s()->executeBusinessRulesForModelOperation('myModel', 'create', (object) []);
+
+        $this->assertEquals(2, $context->counter);
+        $this->assertEquals(3, $context->value);
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Registered business rule must be a callable for');
+        $this->s()->register('X003', 'my third not callable business rule', 'uncallable', ['model' => 'myModel', 'operation' => 'create', 'tenant' => ['testtenant' => false]]);
+
+        $this->expectExceptionMessage('Registered business rule must be a callable for');
+        $this->s()->register('X003', 'my fours existing business rule', $brX002, ['model' => 'myModel', 'operation' => 'create', 'tenant' => ['testtenant' => false]]);
+
+    }
+    /**
+     * @group unit
+     */
+    public function testGetFlattenBusinessRuleDefinitions()
+    {
+        $this->getRegisteredService();
+        $this->assertCount(5, $this->s()->getFlattenBusinessRuleDefinitions());
+    }
+    /**
+     * @group unit
+     */
+    public function testExecuteBusinessRulesForModelOperationWithExecutionContext()
+    {
+        $context = $this->getRegisteredService();
+
+        $validationContext = new ValidationContext($this->mockedErrorManager());
+        $this->s()->executeBusinessRulesForModelOperationWithExecutionContext($validationContext,'myModel', 'create',(object) [], array());
+        $this->assertEquals(3, $context->counter);
+        $this->assertEquals(4, $context->value);
+
+        $this->s()->executeBusinessRulesForModelOperationWithExecutionContext($validationContext,'myModel', '*',(object) [], array());
+        $this->assertEquals(3, $context->counter);
+        $this->assertEquals(9, $context->value);
+
+
+        $this->s()->executeBusinessRulesForModelOperationWithExecutionContext($validationContext,'myModel', 'update',(object) [], array());
+        $this->assertEquals(8, $context->counter);
+        $this->assertEquals(15, $context->value);
+    }
+    /**
+     * @group unit
+     */
+    public function testExecuteBusinessRuleById()
+    {
+        $context = $this->getRegisteredService();
+
+        $this->s()->executeBusinessRuleById('X001');
+        $this->assertEquals(1, $context->counter);
+        $this->s()->executeBusinessRuleById('X002');
+        $this->assertEquals(2, $context->counter);
+        $this->expectExceptionMessage('Unknown business rule \'X003\'');
+        $this->s()->executeBusinessRuleById('X003');
+
+    }
+    /**
+     * @group unit
+     */
+    public function testExecuteModelOperation()
+    {
+        $context = $this->getRegisteredService();
+        $this->s()->executeModelOperation('myModel','delete',(object) []);
+        $this->assertEquals(2, $context->counter);
+        $this->assertEquals(45, $context->value);
+    }
+    /**
+     * @group unit
+     */
+    public function testGetModelBusinessRules()
+    {
+        $this->getRegisteredService();
+        $bs = $this->s()->getModelBusinessRules('myModel');
+        $this->assertCount(1, $bs['delete']);
+        $this->assertCount(2, $bs['create']);
+    }
+    /**
+     *
+     */
+    private function getRegisteredService()
+    {
         $this->mockedTenantService()->expects($this->any())->method('getCurrent')->will($this->returnValue('testtenant'));
         $context = (object) ['counter' => 0, 'value' => 0];
 
         $brX001 = function () use ($context) {
             $context->counter++;
-            $context->value += 1;
+            $context->value += 2;
         };
         $brX002 = function () use ($context) {
             $context->counter++;
             $context->value /= 2;
         };
+        $brX004 = function () use ($context) {
+            $context->counter++;
+            $context->value += 3;
+        };
+        $brX005 = function () use ($context) {
+            $context->counter++;
+            $context->value += 42;
+        };
+        $brX006 = function () use ($context) {
+            $context->counter++;
+            $context->value = 100;
+        };
 
-        $this->s()->register('X001', 'my first business rule', $brX001, ['model' => 'myModel', 'operation' => 'create', 'tenant' => ['testtenant' => false]]);
+        $this->s()->register('X001', 'my first business rule', $brX001,  ['model' => 'myModel', 'operation' => 'create', 'tenant' => ['testtenant' => false]]);
         $this->s()->register('X002', 'my second business rule', $brX002, ['model' => 'myModel', 'operation' => 'create']);
+        $this->s()->register('X004', 'my fourth business rule', $brX004, ['model' => 'myModel', 'operation' => '*', 'tenant' => ['testtenant' => true]]);
+        $this->s()->register('X005', 'my fifth business rule', $brX005,  ['model' => 'myModel', 'operation' => 'delete', 'tenant' => ['testtenant' => true]]);
+        $this->s()->register('X006', 'my sixth business rule',  $brX006, ['model' => '*',       'operation' => 'update']);
 
-        $this->s()->executeBusinessRulesForModelOperation('myModel', 'create', (object) []);
-
-        $this->assertEquals(1, $context->counter);
-        $this->assertEquals(0, $context->value);
+        return $context;
     }
+
+
 }
