b2e9ce3efe577589665350aff475ae271b3ddd36
Olivier Hoareau <olivier@phppro.fr>
2017-10-07 17:39:34 +0200

    dynamic generated tests

---
+++ tests/Itq/Common/Plugin/CodeGenerator/Sdk/Crud/StreamCrudSdkCodeGeneratorTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the COMMON package.
+ *
+ * (c) itiQiti SAS <opensource@itiqiti.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Itq\Common\Plugin\CodeGenerator\Sdk\Crud;
+
+use Itq\Common\Plugin\CodeGenerator\Sdk\Crud\StreamCrudSdkCodeGenerator;
+use Itq\Common\Tests\Plugin\CodeGenerator\Sdk\Base\AbstractSdkCodeGeneratorTestCase;
+
+/**
+ * @author itiQiti Dev Team <opensource@itiqiti.com>
+ *
+ * @group plugins
+ * @group plugins/code-generators
+ * @group plugins/code-generators/sdks
+ * @group plugins/code-generators/sdks/cruds
+ * @group plugins/code-generators/sdks/cruds/stream
+ */
+class StreamCrudSdkCodeGeneratorTest extends AbstractSdkCodeGeneratorTestCase
+{
+    /**
+     * @return StreamCrudSdkCodeGenerator
+     */
+    public function o()
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+
+        return parent::o();
+    }
+    /**
+     * @return array
+     */
+    public function constructor()
+    {
+        return [];
+    }
+}
