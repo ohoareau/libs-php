b507a2d70dff710761626685a51a76f8911f5b38
augustin merle <augustin@laveaucoupet.fr>
2017-10-09 12:19:42 +0200

    complete DynamicUrlServiceTest

--- tests/Itq/Common/Service/DynamicUrlServiceTest.php
+++ tests/Itq/Common/Service/DynamicUrlServiceTest.php
@@ -28,7 +28,6 @@ class DynamicUrlServiceTest extends AbstractServiceTestCase
     public function s()
     {
         /** @noinspection PhpIncompatibleReturnTypeInspection */
-
         return parent::s();
     }
     /**
@@ -37,5 +36,34 @@ class DynamicUrlServiceTest extends AbstractServiceTestCase
     public function constructor()
     {
         return [$this->mockedGeneratorService()];
+    }
+
+    /**
+     * @group unit
+     *
+     * @param string $type
+     * @param array  $def
+     * @param array  $return
+     *
+     * @dataProvider getComputeData
+     */
+    public function testCompute($type, $def, $return)
+    {
+        $def += ['type' => $type];
+        $this->mockedGeneratorService()->expects($this->any())->method('generate')->with('dynamicurl', $return);
+        $this->s()->compute((object) ['foo' => (object) ['bar' => 1]], $def, array());
+    }
+
+    /**
+     * @return array
+     */
+    public function getComputeData()
+    {
+        return [
+            '1 - Test simple param' => ['%app_app_website_premium_url%/g/{bar}', ['vars' => ['bar' => 'myStringUrl' ]], ['dynamicPattern' =>'%app_app_website_premium_url%/g/myStringUrl', 'bar' => 'myStringUrl'  ] ],
+            '2 - Test no param' => ['%app_app_website_premium_url%/g/{foo}', [] ,['dynamicPattern' => '%app_app_website_premium_url%/g/{foo}'] ],
+            '3 - Test undefined pointer param' => ['%app_app_website_premium_url%/g/{bar}', ['vars' => ['bar' => '@myStringUrl.bar' ]], ['dynamicPattern' => '%app_app_website_premium_url%/g/myStringUrl', 'bar' => 'myStringUrl' ] ],
+            '4 - Test pointer param' => ['%app_app_website_premium_url%/g/{foo}', ['vars' => ['foo' => '@foo.bar' ]], ['dynamicPattern' => '%app_app_website_premium_url%/g/1', 'foo' => 1] ],
+        ];
     }
 }
